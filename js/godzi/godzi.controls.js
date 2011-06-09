f0aaf6796c51536ac35d6abcb5d1a4f7663e75d0
Jeff Smith <jeffsmith@gmail.com>
2011-06-09 10:32:34 -0400

    Added GeoRSS support and demo

--- js/godzi/godzi.controls.js
+++ js/godzi/godzi.controls.js
@@ -1,5 +1,7 @@
 godzi.controls = {}
 
+//...................................................................
+
 godzi.controls.LayerSwitcher = function(element_id, map) {
     this.id = element_id;
     this.element = jQuery("#" + element_id);
@@ -59,5 +61,59 @@ godzi.controls.LayerSwitcher.prototype = {
                }
          });
     }    
+  }
+};
+
+//...................................................................
+
+godzi.controls.GeoRSSList = function(element_id, mapView, classid) {
+    this.id = element_id;
+    this.element = $("#" + element_id);
+    this.mapView = mapView;
+	this.classid = classid
+	this.items = undefined;
+    this.init();
+};
+godzi.controls.GeoRSSList.prototype = {
+  init : function() {
+    this.element.append('...');
+  },
+  
+  setItems: function(items) {
+    this.items = items;
+	this.renderList();
+  },
+  
+  renderList: function() {
+    this.element.empty();
+	
+	var mapView = this.mapView;
+	var element = this.element;
+	var classid = this.classid;
+    
+	$.each(this.items, function(i, value) {
+	  var itemDiv;
+	  if (classid == undefined)
+	  {
+	    itemDiv = $('<div style="padding: 4px;' + (i == 0 ? '' : ' border-top: 1px dotted #999;') + '">' + value.title + (value.link == undefined || value.link.length < 0 ? '' : '...<a href="' + value.link + '">Details</a>') + '</div>');
+		$(itemDiv).hover(
+		  function() {
+		    $(this).css("color", "#09f");
+		  },
+		  function() {
+		    $(this).css("color", "");
+		  });
+	  }
+	  else
+	  {
+		itemDiv = $('<div class="' + classid + '">' + value.title + (value.link == undefined || value.link.length < 0 ? '' : '...<a href="' + value.link + '">Details</a>') + '</div>');
+	  }
+		
+	  $(itemDiv).click(function(){
+	      mapView.viewer.manipulator.setViewpoint(value.latitude, value.longitude, 0.0, 0, -90, 2000000);
+	  });
+	  
+	  element.append(itemDiv);
+	});
   }
 };
--- js/godzi/godzi.data.js
+++ js/godzi/godzi.data.js
@@ -49,3 +49,134 @@ godzi.ArcGISImageLayer = function(settings) {
     this.url = settings.url;
     
 };
+
+//...................................................................
+
+godzi.GeoRSSReader = function(url, rate, updateCallback) {
+    this.url = url;
+	
+	this.callbacks = new Array;
+	if (updateCallback != undefined)
+	  this.callbacks.push(updateCallback);
+	
+	this.updateFeed();
+	this.setRate(rate);
+};
+
+godzi.GeoRSSReader.prototype = {
+    updateFeed: function() {
+	    this.items = new Array;
+		
+		if (this.url != undefined)
+		{
+		    var items = this.items;
+			var callbacks = this.callbacks;
+			
+			$.ajax(
+			{
+			  url:this.url,
+			  type: "GET",
+			  //dataType: "xml",
+			  
+			  success: function(data)
+			  {
+			    var selector = $(data).find('item').length > 0 ? 'item' : 'entry';
+			    $(data).find(selector).each(function(i){
+					var lat = undefined;
+					var lon = undefined;
+					
+				    var point = $(this).find('georss\\:point').text();
+					if (point != "")
+					{
+					    lat = point.split(" ")[0];
+					    lon = point.split(" ")[1];
+					}
+					else
+					{
+					    lat = $(this).find('geo\\:lat').text();
+					    lon = $(this).find('geo\\:long').text();
+					}
+					
+					var links = this.getElementsByTagName("link");
+					
+					items.push({ guid: $(this).find('guid').text(),
+					             title: $(this).find('title').text(),
+								 author: $(this).find('author').text(),
+								 pubDate: $(this).find('pubDate').text(),
+								 description: $(this).find('description').text(),
+								 link: $(this).find('link').eq(0).attr('href'),
+								 latitude: lat,
+								 longitude: lon,
+								 src: $(this).get() });
+				});
+				
+				for (var i in callbacks)
+				{
+				  var callback = callbacks[i];
+				  callback(items);
+				}
+			  },
+			  
+			  error: function(jqXHR, status, error)
+			  {
+				//alert("Eror reading RSS feed: " + status);
+				for (var i in callbacks)
+				{
+				  var callback = callbacks[i];
+				  callback(items);
+				}
+			  }
+			});
+		}
+	},
+	
+	setRate: function(newRate) {
+	    if (this.interval != undefined)
+	        window.clearInterval(this.interval);
+			
+		this.rate = newRate;
+		if (this.rate > 0)
+	        this.interval = window.setInterval(function(layer) { layer.updateFeed(); }, this.rate * 1000, this);
+	},
+	
+	addCallback: function(updateCallback) {
+	    if (updateCallback != undefined)
+		{
+		  this.callbacks.push(updateCallback);
+		  updateCallback(this.items);
+		}
+	}
+};
+
+//...................................................................
+
+godzi.GeoRSSLayer = function(mapView, url, rate, iconUrl) {
+    this.mapView = mapView;
+    this.url = url;
+	this.iconUrl = iconUrl != undefined ? iconUrl : "http://google-maps-icons.googlecode.com/files/redblank.png";
+	
+	this.positionEngine = new godzi.PositionEngine(mapView);
+	
+	var thisObj = this;
+	this.reader = new godzi.GeoRSSReader(url, rate, function(items) { thisObj.createIcons(items); });
+};
+
+godzi.GeoRSSLayer.prototype = {
+    setRate: function(newRate) {
+        this.reader.setRate(newRate);	
+	},
+	
+	createIcons: function(items) {
+	    this.positionEngine.elements = [];
+		
+		for (var i in items)
+		{
+		    var icon = new godzi.Icon("icon" + i + "_" + items[i].guid, osgearth.deg2rad(items[i].longitude), osgearth.deg2rad(items[i].latitude), 0, this.iconUrl, {
+              width: 32,
+              height: 32,
+			  title: items[i].title
+            });
+            this.positionEngine.elements.push( icon );   
+		}
+	}
+};
