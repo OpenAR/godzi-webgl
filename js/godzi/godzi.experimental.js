be38801190180075fbedefaf3e0949fd8172209c
Jeff Smith <jeffsmith@gmail.com>
2011-06-15 15:34:50 -0400

    Added WOEID weather demo. Minor updates to the rss reader and PlaceSearch.

--- js/godzi/godzi.experimental.js
+++ js/godzi/godzi.experimental.js
@@ -47,31 +47,32 @@ godzi.PlaceSearch.doSearch = function(place, callback)
 
       success: function(data)
 	  {
-        var xml = data.documentElement;
-		if (xml != undefined)
+		var xml = data.documentElement;
+
+		try
 		{
-		  var lat = xml.getElementsByTagName("latitude")[0].firstChild.nodeValue;
-		  var lon = xml.getElementsByTagName("longitude")[0].firstChild.nodeValue;
+			var lat = xml.getElementsByTagName("latitude")[0].firstChild.nodeValue;
+			var lon = xml.getElementsByTagName("longitude")[0].firstChild.nodeValue;
 
-		  var southWest = xml.getElementsByTagName("southWest")[0];
-		  var swlat = southWest.getElementsByTagName("latitude")[0].firstChild.nodeValue;
-		  var swlon = southWest.getElementsByTagName("longitude")[0].firstChild.nodeValue;
+			var southWest = xml.getElementsByTagName("southWest")[0];
+			var swlat = southWest.getElementsByTagName("latitude")[0].firstChild.nodeValue;
+			var swlon = southWest.getElementsByTagName("longitude")[0].firstChild.nodeValue;
 
-		  var northEast = xml.getElementsByTagName("northEast")[0];
-		  var nelat = northEast.getElementsByTagName("latitude")[0].firstChild.nodeValue;
-		  var nelon = northEast.getElementsByTagName("longitude")[0].firstChild.nodeValue;
-		  
-		  callback(lat, lon, swlat, swlon, nelat, nelon);
+			var northEast = xml.getElementsByTagName("northEast")[0];
+			var nelat = northEast.getElementsByTagName("latitude")[0].firstChild.nodeValue;
+			var nelon = northEast.getElementsByTagName("longitude")[0].firstChild.nodeValue;
+			  
+			callback(lat, lon, swlat, swlon, nelat, nelon, data);
 		}
-		else
+		catch (e)
 		{
-		  alert("Cannot find that location.");
+			callback(0,0,0,0,0,0,"Cannot find location: " + place);
 		}
       },
 	  
 	  error: function(jqXHR, status, error)
 	  {
-	    alert("ERROR: " + status);
+		callback(0,0,0,0,0,0,status);
       }
 	});
   }
@@ -327,3 +328,127 @@ godzi.PositionEngine.prototype = {
     }
   }
 }
+
+//........................................................................
+
+godzi.WOEIDWeatherLayer = function(mapView, places, proxy, iconOptions) {
+    this.positionEngine = new godzi.PositionEngine(mapView);
+	this.places = places;
+	this.proxy = proxy;
+	
+	var defaults = {
+	  url: "http://google-maps-icons.googlecode.com/files/cloudsun.png",
+      width: 32,
+      height: 32,
+      class: "",
+	  renderer: undefined
+    };
+    this.options = jQuery.extend({}, defaults, iconOptions);
+	
+	this.readers = [];
+	this.icons = [];
+	this.init();
+};
+
+godzi.WOEIDWeatherLayer.prototype = {
+    init: function() {
+		for (var i in this.places)
+		{
+		    var place = this.places[i];
+			var thisObj = this;
+			godzi.PlaceSearch.doSearch(place, function(lat, lon, swlat, swlon, nelat, nelon, data) {
+			    var woeid = $(data).find('woeId').eq(0).text();
+				if (woeid != undefined && woeid != '')
+				  thisObj.createReader(woeid);
+			});
+		}
+	},
+	
+	createReader: function(id) {
+	    var url = this.proxy + 'http://weather.yahooapis.com/forecastrss?w=' + id;
+		var thisObj = this;
+		var renderer = this.options.renderer;
+		this.readers[id] = new godzi.GeoRSSReader(url, 60, function(items) {
+		    if (renderer != undefined)
+			    renderer(items[0], id);
+			else
+			    thisObj.createIcon(items[0], id);
+		});
+	},
+	
+	createIcon: function(item, id) {
+		var active = false;
+	    if (this.icons[id] != undefined)
+		{
+		    if (this.icons[id].popup != undefined)
+			{
+				this.positionEngine.removeElement(icons[id].popup);
+				active = true;
+			}
+				
+		    this.positionEngine.removeElement(this.icons[id]);
+			this.icons[id] = undefined;
+		}
+		
+	    var icon = new godzi.Icon("icon" + id, Math.deg2rad(item.longitude), Math.deg2rad(item.latitude), 0, this.options.url, {
+		  width: this.options.width,
+		  height: this.options.height,
+		  class: this.options.class,
+		  title: item.title
+		});
+		
+		icon.offset = [this.options.width / -2, this.options.height / -2];
+		
+		if (active)
+		{
+			this.createIconPopup(icon, id, item.latitude, item.longitude, item.title, item.description, item.link);
+		}
+		
+		var thisObj = this;
+		icon.element.bind("click", {url: item.link,
+									title: item.title,
+									engine: this.positionEngine,
+									lat: item.latitude,
+									lon: item.longitude,
+									description: item.description,
+									icon: icon,
+									id: id
+									}, function(e) {
+			  if (e.data.icon.popup != undefined)
+			  {
+			    e.data.engine.removeElement(e.data.icon.popup);
+				e.data.icon.popup = undefined;
+			  }
+			  else
+			  {
+				  thisObj.createIconPopup(e.data.icon, e.data.id, e.data.lat, e.data.lon, e.data.title, e.data.description, e.data.url);
+			  }
+			});
+		
+		this.icons[id] = icon;
+		this.positionEngine.addElement( icon );
+	},
+	
+	createIconPopup: function(icon, id, lat, lon, title, content, url) {
+		//special test for Yahoo weather image
+		var imgData = "";
+	    if ($(content)[0].nodeType == 8)
+			imgData = $(content)[0].data.replace('[CDATA[', '') + '>';
+		
+		var html = '<div class="weather_popup_background"><div class="weather_popup"><h4 class="weather_popup">' + title + '</h4>' + '<div class="weather_popup_image">' + imgData + '</div><br />' + content.replace(']]&gt;', '') + '</div></div>';
+		
+		var htmlElem = $(html);
+		jQuery("body").append(htmlElem);
+		
+		htmlElem[0].onselectstart = function() { return false; }
+		htmlElem[0].onmousedown   = function() { return false; }
+		
+		htmlElem.bind("click", { icon: icon }, function(e) {
+			$(e.data.icon.element).click();
+		});
+		
+		var popup = new godzi.PositionedElement("popup_" + id, Math.deg2rad(lon), Math.deg2rad(lat), 0, {element: htmlElem, vAlign: "bottom"});
+		icon.popup = popup;
+		this.positionEngine.addElement(popup);
+	}
+};
